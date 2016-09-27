9bd6fad763262d7c52602914350f3bde37977886
traversenote <210sound@gmail.com>
2016-09-27 13:25:06 +1300

    Added a placeholder page

---
+++ newTicket.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta http-equiv='content-type' content='text/html; charset=utf-8'>
+<link rel='stylesheet' href='freightRegister.css' />
+<title>Freight Codes</title>
+</head>
+
+<body>
+This function is currently unavailable.
+</body>
+</html>
